5d7f1850dbbf3a82f552ff40091d06ecc43ce090
eya boukhzar <eya.boukhzar>
2024-02-18 23:02:40 +0100

    crud post and comment

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CommentRepository;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/Home', name: 'app_transport')]
+    public function index(): Response
+    {
+        return $this->render('base.html.twig', [
+            'controller_name' => 'TransportController',
+        ]);
+    }
+
+    #[Route('/transportR', name: 'app_transportR')]
+    public function transport(): Response
+    {
+        return $this->render('transport.html.twig', [
+            'controller_name' => 'TransportController',
+        ]);
+    }
+    #[Route('/Forum', name: 'app_Forum')]
+    public function Forum(PostRepository $postRepository,CommentRepository $commentRepository): Response
+    {
+        // Fetch posts from the repository
+        $posts = $postRepository->findAll();
+        return $this->render('forum.html.twig', [
+            'controller_name' => 'TransportController',
+            'posts' => $posts,
+            'comments1' => $commentRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/sign_in', name: 'app_sign_in')]
+    public function sign_in(): Response
+    {
+        return $this->render('sign_in.html.twig', [
+            'controller_name' => 'TransportController',
+        ]);
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(): Response
+    {
+        return $this->render('register.html.twig', [
+            'controller_name' => 'TransportController',
+        ]);
+    }
+
+    #[Route('/back', name: 'app_back')]
+    public function back(): Response
+    {
+        return $this->render('back.html.twig', [
+            'controller_name' => 'TransportController',
+        ]);
+    }
+}
---
+++ src/Form/PostType.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id_post = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "L'auteur ne peut pas être vide")]
+    private ?string $author = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Le titre ne peut pas être vide")]
+    #[Assert\Length(max: 255, maxMessage: "Le titre ne peut pas dépasser {{ limit }} caractères")]
+    private ?string $title = null;
+
+    #[ORM\Column]
+    #[Assert\NotBlank(message: "La date de création ne peut pas être vide")]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    #[Assert\NotNull(message: "Le nombre de vues ne peut pas être vide")]
+    #[Assert\Type(type: "integer", message: "Le nombre de vues doit être un entier")]
+    private ?int $views_count = null;
+
+    #[ORM\OneToMany(mappedBy: 'post', targetEntity: Comment::class)]
+    private Collection $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
+
+    public function getIdPost(): ?int
+    {
+        return $this->id_post;
+    }
+
+    public function setIdPost(int $id_post): static
+    {
+        $this->id_post = $id_post;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getViewsCount(): ?int
+    {
+        return $this->views_count;
+    }
+
+    public function setViewsCount(int $views_count): static
+    {
+        $this->views_count = $views_count;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setPost($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getPost() === $this) {
+                $comment->setPost(null);
+            }
+        }
+
+        return $this;
+    }
+}
